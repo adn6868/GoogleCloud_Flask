8966ef3a69ef1fc4aa111794de354663bd409056
Anh Nguyen_MacbookPro <anhnd32@fsoft.com.vn>
2019-01-27 15:50:22 -0600

    yaml and requirement for GCP deployment

--- comming soon/app.py
+++ comming soon/app.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 @app.route('/static' , methods = ['GET','POST'])
 
 
-
 def index():
 
 	return render_template('index.html')
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=5000, debug=True)
+	app.run(host='127.0.0.1', port=8080, debug=True)
 
