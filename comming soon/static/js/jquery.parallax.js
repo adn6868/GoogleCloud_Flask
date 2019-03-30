7181c79bf3ceeb39fddf01860faf78676f0666e8
Anh Nguyen_MacbookPro <anhdnguyenn@gmail.com>
2019-03-30 16:26:52 -0500

    make code pretty

--- comming soon/static/js/jquery.parallax.js
+++ comming soon/static/js/jquery.parallax.js
@@ -11,11 +11,11 @@ http://www.opensource.org/licenses/mit-license.php
 http://www.gnu.org/licenses/gpl.html
 */
 
-(function( $ ){
+(function($) {
 	var $window = $(window);
 	var windowHeight = $window.height();
 
-	$window.resize(function () {
+	$window.resize(function() {
 		windowHeight = $window.height();
 	});
 
@@ -24,15 +24,13 @@ http://www.gnu.org/licenses/gpl.html
 		var getHeight;
 		var firstTop;
 		var paddingTop = 0;
-		
-		//get the starting position of each element to have parallax applied to it	
-		function update (){
-			
-			$this.each(function(){
-								
+
+		//get the starting position of each element to have parallax applied to it
+		function update() {
+			$this.each(function() {
 				firstTop = $this.offset().top;
 			});
-	
+
 			if (outerHeight) {
 				getHeight = function(jqo) {
 					return jqo.outerHeight(true);
@@ -42,30 +40,29 @@ http://www.gnu.org/licenses/gpl.html
 					return jqo.height();
 				};
 			}
-				
+
 			// setup defaults if arguments aren't specified
-			if (arguments.length < 1 || xpos === null) xpos = "50%";
+			if (arguments.length < 1 || xpos === null) xpos = '50%';
 			if (arguments.length < 2 || speedFactor === null) speedFactor = 0.5;
 			if (arguments.length < 3 || outerHeight === null) outerHeight = true;
-			
+
 			// function to be called whenever the window is scrolled or resized
-			
-				var pos = $window.scrollTop();				
-	
-				$this.each(function(){
-					var $element = $(this);
-					var top = $element.offset().top;
-					var height = getHeight($element);
-	
-					// Check if totally above or totally below viewport
-					if (top + height < pos || top > pos + windowHeight) {
-						return;
-					}
-					
-					$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
-					
-				});
-		}		
+
+			var pos = $window.scrollTop();
+
+			$this.each(function() {
+				var $element = $(this);
+				var top = $element.offset().top;
+				var height = getHeight($element);
+
+				// Check if totally above or totally below viewport
+				if (top + height < pos || top > pos + windowHeight) {
+					return;
+				}
+
+				$this.css('backgroundPosition', xpos + ' ' + Math.round((firstTop - pos) * speedFactor) + 'px');
+			});
+		}
 
 		$window.bind('scroll', update).resize(update);
 		update();
--- comming soon/templates/getCommic.py
+++ comming soon/templates/getCommic.py
@@ -1,11 +1,13 @@
 class getURL:
-	def __init__(self):
-		self.URL = 'http://truyentranhtuan.com/'
-		self.setComic = 'one-piece'
-		self.chapter = '-chuong-1/'
+    def __init__(self):
+        self.URL = 'http://truyentranhtuan.com/'
+        self.setComic = 'one-piece'
+        self.chapter = '-chuong-1/'
+
 
 def main():
-	pass
+    pass
+
 
 if __name__ == '__main__':
-	main()
+    main()
